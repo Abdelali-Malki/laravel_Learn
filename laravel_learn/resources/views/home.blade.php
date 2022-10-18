6a993ff88559250f62e72bbc6ec1ae7289488bd3
Abdelali MAlki <abdelalimalki.ae@gmail.com>
2022-10-18 17:02:45 +0100

    commit

---
+++ laravel_learn/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('master.layout')
+
+
+
+@section('title')
+    acceuil
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8">
+                <h1>Hello from home page</h1>
+        </div>
+
+    </div>
+
+@endsection
---
+++ laravel_learn/resources/views/request.blade.php
@@ -0,0 +1,4 @@
+<form action="/user" method="get">
+<input type="text" name="name">
+<button>click</button>
+</form>
